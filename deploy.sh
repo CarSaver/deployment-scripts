e7460b45f3f06ecf545de6ec9eae29af09251156
ayush-s-simformsolutions <ayush.s@simformsolutions.com>
2022-11-23 15:34:26 +0530

    jq change

--- deploy.sh
+++ deploy.sh
@@ -18,12 +18,12 @@ JQ="jq --raw-output --exit-status"
 # Task Definition Template
 curl "https://raw.githubusercontent.com/CarSaver/deployment-scripts/v3.1/ecs_template.base.json" > ecs_template.base.json
 $JQ --raw-output --exit-status -s '.[0][0] * .[1][0]' ecs_template.base.json ecs_template.json | cat <(echo '[') <(cat -) <(echo ']') > ecs_template_new.json
-jq '.[0] += {"memoryReservation":'$ECS_TASK_MEMORY'}' ecs_template_new.json > ecs_template_new.json
+jq '.[0] += {"memoryReservation":'$ECS_TASK_MEMORY'}' ecs_template_new.json > ecs_task_template.json
 
-ECS_TASK_TEMPLATE=$(<ecs_template_new.json) 
+ECS_TASK_TEMPLATE=$(<ecs_task_template.json) 
 
 
-ECS_TASK_TEMPLATE=${ECS_TASK_TEMPLATE?"Unable to load ecs_template_new.json"}
+ECS_TASK_TEMPLATE=${ECS_TASK_TEMPLATE?"Unable to load ecs_task_template.json"}
 
 ECS_TASK_TEMPLATE="${ECS_TASK_TEMPLATE//\"/\\\"}"
 
